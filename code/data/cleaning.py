f2a84707394b30ac5ca4216057555d0279efd0f2
Russel Dsouza <30742639+rshwndsz@users.noreply.github.com>
2019-08-23 13:45:37 +0530

    Add more cleaning methods

--- code/data/cleaning.py
+++ code/data/cleaning.py
@@ -1,4 +1,8 @@
 import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+stop_words = set(stopwords.words('english'))
 
 
 def comment_remover(text):
@@ -19,4 +23,43 @@ def comment_remover(text):
     )
     return re.sub(pattern, replacer, text)
 
+
+def camel_case_split(text):
+    """
+    Split camel case into list
+
+    :return: list of words
+
+    See: https://stackoverflow.com/a/29920015
+    """
+    matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', text)
+    return [m.group(0) for m in matches]
+
+
+def snake_case_split(text):
+    """
+    Split snake case into list
+
+    :return: list
+    """
+    return text.split('_')
+
+
+def remove_common_words(string):
+    if type(string) == list:
+        return filter(lambda w: w not in stop_words, string)
+    elif type(string) == str:
+        return filter(lambda w: w not in stop_words, string.split(' '))
+    else:
+        raise TypeError('Must be string or list')
+
+
+def remove_numbers_and_symbols(string):
+    if type(string) == list:
+        return [i for i in string if i.isalpha()]
+
+
+def stemming(string_list):
+    ps = PorterStemmer()
+    return [ps.stem(i) for i in string_list]
 
