9caa491d546dabda32e0792c8aff3cbf9b597e25
Russel Dsouza <russel.dsouza47@gmail.com>
2019-08-25 20:22:58 +0530

    Update config

--- code/config/config.py
+++ code/config/config.py
@@ -14,9 +14,13 @@ val_freq = 1
 resume_from_epoch = 0
 min_val_loss = 1000
 
+batch_size = 8
+n_epochs = 10
+initial_lr = 3e-4
+
 
 # Data-specific
 project_root = '/Users/Russel/myProjects/dl-for-blockchain/code/'
 dataset_root = os.path.join(project_root, 'dataset')
-model_path = os.path.join(project_root, 'checkpoints', 'model_best.pth')
+model_path_root = os.path.join(project_root, 'checkpoints')
 results_dir = os.path.join(project_root, 'results')
