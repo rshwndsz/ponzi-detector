3b0d89785602f7ca46013e31c784943d79cb1e0e
Russel Dsouza <russel.dsouza47@gmail.com>
2019-08-25 19:04:01 +0530

    Add DS1 and DS2 representation

--- code/data/representation.py
+++ code/data/representation.py
@@ -1,24 +1,38 @@
+import numpy as np
+import torch
 import torch.nn as nn
 from torch.utils import data
+import pandas as pd
+
+from code.data.preprocessing import preprocess
+from code.data.preprocessing import bytecode_to_image
 
 
 class BytecodeTokenDataset(data.Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, csv_path):
+        df = pd.read_csv(csv_path)
+        df = preprocess(df)
+        self.bytecodes = df['bytecode']
+        self.tokens = df['tokens']
 
     def __getitem__(self, idx):
-        pass
+        return self.bytecodes[idx], self.tokens[idx]
 
     def __len__(self):
-        pass
+        return len(self.tokens)
 
 
 class BytecodePonziDataset(data.Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, csv_path):
+        df = pd.read_csv(csv_path)
+        result = pd.DataFrame(columns=['address', 'bytecode'])
+        for i, row in df.iterrows():
+            result['bytecode'].append(bytecode_to_image(row['bytecode']))
+        df = result
+        self.bytecodes = df['bytecode']
 
     def __getitem__(self, idx):
-        pass
+        return self.bytecodes[idx]
 
     def __len__(self):
-        pass
+        return len(self.bytecodes)
