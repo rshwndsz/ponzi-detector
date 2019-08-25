1e1d08a769a4d9ea76ca9c09e398b0f0996cb11f
Russel Dsouza <russel.dsouza47@gmail.com>
2019-08-25 17:28:15 +0530

    Init dataset representations

---
+++ code/data/representation.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+from torch.utils import data
+
+
+class BytecodeTokenDataset(data.Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def __len__(self):
+        pass
+
+
+class BytecodePonziDataset(data.Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def __len__(self):
+        pass
